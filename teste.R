9e02d62ec9e77713b70a3a0cefc180822744ca64
Thales Melo <85687910+Thales-Melo@users.noreply.github.com>
2024-03-26 13:42:26 -0300

    Update teste.R

--- teste.R
+++ teste.R
@@ -8,6 +8,11 @@ link <- "https://www.chancedegol.com.br/rkclub.htm"
 
 webpage <- read_html(link)
 
+tabela <- webpage %>% html_table(header = T)
+
+print(tabela)
+
+view(tabela)
 
 names <- webpage %>% html_nodes("img+ font") %>% html_text()
 
@@ -31,9 +36,43 @@ media <- mean(pontuacao_num)
 
 print(media)
 
+write_excel_csv(data, file = "teste.csv")
 
 view(data)
 
+#####################################################
 
-# view(names
+o_que_procuro_para_susbtituir <- "palavra"
+o_que_quero_substituir_por <- "batata"
+meu_texto <- "quero substituir essa palavra"
+
+texto_final <- stringr::str_replace( meu_texto, o_que_procuro_para_susbtituir, o_que_quero_substituir_por)
+
+print(texto_final)
+
+
+
+# TRATAMENTO DE VALORES AUSENTES
+
+# Remover linhas ou colunas com valores ausentes: 
+# Você pode usar a função na.omit() para remover
+# linhas ou colunas que contenham valores ausentes.
+dados_limpos <- na.omit(dados)
+
+
+# Preencher valores ausentes com um valor específico:
+# Você pode usar a função na.fill() para preencher
+# os valores ausentes com um valor específico.
+dados_sem_na <- na.fill(dados, value = 0)
+
+
+# DUPLICATAS
+
+# Para lidar com valores duplicados nos seus dados, 
+# você pode usar a função unique() para remover duplicatas
+# e a função duplicated() para identificar duplicatas.
+dados_sem_duplicatas <- unique(dados)
+
+linhas_duplicadas <- dados[duplicated(dados), ]
+
 
